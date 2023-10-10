816d23e784c20f33af3a56febd1dd378ee0dfbd1
rbverissimo <rverissimo.dev@gmail.com>
2023-10-09 23:16:39 -0300

    Primeiro commit do projeto

---
+++ app/Models/Pessoa.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pessoa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pessoas';
+
+    protected $fillable = ['nome', 
+    'cpf', 
+    'profissao', 
+    'telefone_celular', 
+    'telefone_fixo', 
+    'telefone_trabalho'];
+}
