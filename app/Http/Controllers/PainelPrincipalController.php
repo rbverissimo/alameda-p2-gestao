61e2a0609e8874ca4ca18a5777d31cb518889442
rbverissimo <rverissimo.dev@gmail.com>
2023-12-18 07:02:27 -0300

    Reajustando o projeto do app da P2

--- app/Http/Controllers/PainelPrincipalController.php
+++ app/Http/Controllers/PainelPrincipalController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ImovelController extends Controller
+class PainelPrincipalController extends Controller
 {
     //
 }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pessoas', [PessoaController::class, 'index']);
+
