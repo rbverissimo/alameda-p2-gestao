f11daf0f2a736bb38c92f69decfc52e54e2f325b
rbverissimo <rverissimo.dev@gmail.com>
2023-12-27 06:33:08 -0300

    Ajustando o controller para salvar as requisições corretamente

--- app/Http/Controllers/CalculoContasController.php
+++ app/Http/Controllers/CalculoContasController.php
@@ -9,56 +9,56 @@ class CalculoContasController extends Controller
 {
     public function calculoContas(Request $request) {
 
-        $conta_imovel = new ContaImovel();
-        $conta_imovel->salacodigo = 1;
-        $conta_imovel->valor = $request->input('conta-luz-1');
-        $conta_imovel->ano = $request->input('ano-referencia');
-        $conta_imovel->mes = $request->input('mes-referencia');
-        $conta_imovel->dataVencimento = $request->input('data-vencimento-conta-luz-1');
-        $conta_imovel->referenciaConta = $request->input('referenciaConta-conta-luz-1');
-        $conta_imovel->nrDocumento = $request->input('nr-documento-conta-luz-1');
-        $conta_imovel->tipocodigo = 2;
+        if($request->isMethod('post')){
+            $conta_imovel = new ContaImovel();
+            $conta_imovel->salacodigo = 1;
+            $conta_imovel->valor = $request->input('conta-luz-1');
+            $conta_imovel->ano = $request->input('ano-referencia');
+            $conta_imovel->mes = $request->input('mes-referencia');
+            $conta_imovel->dataVencimento = $request->input('data-vencimento-conta-luz-1');
+            $conta_imovel->referenciaConta = $request->input('referenciaConta-conta-luz-1');
+            $conta_imovel->nrDocumento = $request->input('nr-documento-conta-luz-1');
+            $conta_imovel->tipocodigo = 2;
 
-        $conta_imovel->save();
+            $conta_imovel->save();
 
-        $c2 = new ContaImovel();
-        $c2->salacodigo = 2;
-        $c2->valor = $request->input('conta-luz-2');
-        $c2->ano = $request->input('ano-referencia');
-        $c2->mes = $request->input('mes-referencia');
-        $c2->dataVencimento = $request->input('data-vencimento-conta-luz-2');
-        $c2->referenciaConta = $request->input('referenciaConta-conta-luz-2');
-        $c2->nrDocumento = $request->input('nr-documento-conta-luz-2');
-        $c2->tipocodigo = 2;
+            $c2 = new ContaImovel();
+            $c2->salacodigo = 2;
+            $c2->valor = $request->input('conta-luz-2');
+            $c2->ano = $request->input('ano-referencia');
+            $c2->mes = $request->input('mes-referencia');
+            $c2->dataVencimento = $request->input('data-vencimento-conta-luz-2');
+            $c2->referenciaConta = $request->input('referenciaConta-conta-luz-2');
+            $c2->nrDocumento = $request->input('nr-documento-conta-luz-2');
+            $c2->tipocodigo = 2;
 
-        $conta_imovel->save();
+            $c2->save();
 
-        $c3 = new ContaImovel();
-        $c3->salacodigo = 3;
-        $c3->valor = $request->input('conta-luz-3');
-        $c3->ano = $request->input('ano-referencia');
-        $c3->mes = $request->input('mes-referencia');
-        $c3->dataVencimento = $request->input('data-vencimento-conta-luz-3');
-        $c3->referenciaConta = $request->input('referenciaConta-conta-luz-3');
-        $c3->nrDocumento = $request->input('nr-documento-conta-luz-3');
-        $c3->tipocodigo = 2;
+            $c3 = new ContaImovel();
+            $c3->salacodigo = 3;
+            $c3->valor = $request->input('conta-luz-3');
+            $c3->ano = $request->input('ano-referencia');
+            $c3->mes = $request->input('mes-referencia');
+            $c3->dataVencimento = $request->input('data-vencimento-conta-luz-3');
+            $c3->referenciaConta = $request->input('referenciaConta-conta-luz-3');
+            $c3->nrDocumento = $request->input('nr-documento-conta-luz-3');
+            $c3->tipocodigo = 2;
 
-        $conta_imovel->save();
+            $c3->save();
 
-        $c_agua = new ContaImovel();
-        $c_agua->imovelcodigo = 1;
-        $c_agua->valor = $request->input('conta-agua');
-        $c_agua->ano = $request->input('ano-referencia');
-        $c_agua->mes = $request->input('mes-referencia');
-        $c_agua->dataVencimento = $request->input('data-vencimento-conta-agua');
-        $c_agua->referenciaConta = $request->input('referenciaConta-conta-agua');
-        $c_agua->nrDocumento = $request->input('nr-documento-conta-agua');
-        $c_agua->tipocodigo = 1;
+            $c_agua = new ContaImovel();
+            $c_agua->imovelcodigo = 1;
+            $c_agua->valor = $request->input('conta-agua');
+            $c_agua->ano = $request->input('ano-referencia');
+            $c_agua->mes = $request->input('mes-referencia');
+            $c_agua->dataVencimento = $request->input('data-vencimento-conta-agua');
+            $c_agua->referenciaConta = $request->input('referenciaConta-conta-agua');
+            $c_agua->nrDocumento = $request->input('nr-documento-conta-agua');
+            $c_agua->tipocodigo = 1;
 
-        $conta_imovel->save();
-
-
-
+            $c_agua->save();
+        }
+        
         return view('app.calculo-contas');
     }
 }
