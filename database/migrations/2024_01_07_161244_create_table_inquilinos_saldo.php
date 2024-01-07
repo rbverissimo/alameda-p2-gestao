e3f65897641c8bedf3fc0354abd973ded94e72ae
rbverissimo <rverissimo.dev@gmail.com>
2024-01-07 13:20:31 -0300

    Adicionando uma migration nova ao projeta. Essa cria a tabela inquilinos_saldo que vai mapear o saldo dos inquilinos, auxiliando na construção da situação financeira dos mesmos

---
+++ database/migrations/2024_01_07_161244_create_table_inquilinos_saldo.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableInquilinosSaldo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_inquilinos_saldo', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('inquilinocodigo')->unique();
+            $table->double('saldo_atual');
+            $table->double('saldo_anterior')->nullable();
+            $table->string('observacoes', 400)->nullable();
+
+            $table->foreign('inquilinocodigo')->references('id')->on('inquilinos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('inquilinos_saldo', function(Blueprint $table){
+            $table->dropForeign('inquilinos_saldo_inquilinocodigo_foreign');
+        });
+
+        Schema::dropIfExists('table_inquilinos_saldo');
+    }
+}
