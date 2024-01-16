ef6776138c749a8addecaacf3082b2b11363a4b3
rbverissimo <rverissimo.dev@gmail.com>
2024-01-16 06:58:52 -0300

    Query para buscar as contas para o cálculo realizada

--- app/Services/CalculoContasService.php
+++ app/Services/CalculoContasService.php
@@ -8,7 +8,13 @@ class CalculoContasService {
 
     public function calcularContasInquilinos(){
 
-        $conta_agua = ContaImovel::max('referenciaConta')->where('tipocodigo', 1);
+        $conta_agua = ContaImovel::where('tipocodigo', 1)->orderByDesc('id')->limit(1);
+
+        $conta_luz_sala1 = ContaImovel::where('tipocodigo', 2)->where('salacodigo', 1)->orderByDesc('id')->limit(1)->get();
+
+        $conta_luz_sala2 = ContaImovel::where('tipocodigo', 2)->where('salacodigo', 2)->orderByDesc('id')->limit(1)->get();
+
+        $conta_luz_sala3 = ContaImovel::where('tipocodigo', 2)->where('salacodigo', 3)->orderByDesc('id')->limit(1)->get();
     }
 
 }
