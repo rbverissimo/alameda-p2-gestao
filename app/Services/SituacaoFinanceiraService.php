775dc6b3aa8d324c2ff4771797ebdd8a8d342814
rbverissimo <rverissimo.dev@gmail.com>
2024-01-21 08:59:41 -0300

    Tornando a classe de serviço mais limpa

--- app/Services/SituacaoFinanceiraService.php
+++ app/Services/SituacaoFinanceiraService.php
@@ -8,48 +8,28 @@ class SituacaoFinanceiraService {
 
       public function buscarSituacaoFinanceira($inquilino_id, $referencia){
 
-            // inquilino
-            // referencia
             $ano = ProjectUtils::getAnoFromReferencia($referencia);
             $mes = ProjectUtils::getMesFromReferencia($referencia);
 
-
-            // descobrir a sala do inquilino 
-            $sala_inquilino = Inquilino::select('salacodigo')->where('id', $inquilino_id);
-
-            //Buscar uma collection de contas daquele ano-mês
-            $contas = ContaImovel::where('ano', $ano)->where('mes', $mes)
-                  ->orderByDesc('id')
-                  ->get();
-            
-            $contas_agua = ProjectUtils::getContasByTipo($contas, 1);
-            
-            $contas_luz = ContasUtils::create($contas)
-                  ->filterByParam('tipocodigo', 2)
-                  ->filterByParam('salacodigo', $sala_inquilino)
-                  ->getContas();
-
-            $max_id_agua = ProjectUtils::getMaxID($contas_agua);
-            $max_id_luz = ProjectUtils::getMaxID($contas_luz);
-
-            // buscar todas as contas do imóvel do mês com id máximo para aquela referência
-            
-
-            // buscar as contas do inquilino ligadas aquelas contas do imóvel
-
-            // montar um objeto e retorna-lo; 
-
-            $conta_luz = InquilinoConta::select('valorinquilino')
-            ->join('contas_imoveis', 'inquilinos_contas.contacodigo', '=', 'contas_imoveis.id')
-            ->where('contas_imoveis.tipocodigo', 2)
-            ->where('inquilinos_contas.inquilinocodigo', $inquilino_id)
-            ->orderByDesc('inquilinos_contas.id')
-            ->first();
+            $conta_luz = $this->getValorInquilinoBy(2, $inquilino_id, $ano, $mes);
+            $conta_agua = $this->getValorInquilinoBy(1, $inquilino_id, $ano, $mes);
 
             $situacao_financeira = "";
 
             return $situacao_financeira;
 
+      }
+
+      private function getValorInquilinoBy($tipoconta, $inquilino_id, $ano, $mes){
+
+            return InquilinoConta::select('inquilinos_contas.valorinquilino')
+                  ->join('contas_imoveis', 'inquilinos_contas.contacodigo', '=', 'contas_imoveis.id')
+                  ->where('contas_imoveis.tipocodigo', $tipoconta)
+                  ->where('inquilinos_contas.inquilinocodigo', $inquilino_id)
+                  ->where('contas_imoveis.ano', $ano)
+                  ->where('contas_imoveis.mes', $mes)
+                  ->orderByDesc('contas_imoveis.id')
+                  ->first();
       }
 
 }
