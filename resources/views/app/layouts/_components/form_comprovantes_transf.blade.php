ca28de597f34a2bb0785dd4eab2bae2abbe326f3
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2024-04-08 09:26:20 -0300

    Melhorando a UI dos comprovantes

--- resources/views/app/layouts/_components/form_comprovantes_transf.blade.php
+++ resources/views/app/layouts/_components/form_comprovantes_transf.blade.php
@@ -7,46 +7,66 @@
       @isset($comprovante->id)
       <input name="id-comprovante" placeholder="ID" value="{{$comprovante->id}}" type="hidden">
       @endisset
-      <input name="valor-comprovante" id="input-valor-comprovante" type="text" placeholder="Valor do comprovante: "
-      value="{{ isset($comprovante->valor) ?
-            old('valor-comprovante', $comprovante->valor) : old('valor-comprovante') }}">
-      <br>
-      @isset($tipos_comprovantes)
-            <select name="tipo-comprovante">
-                  @foreach ($tipos_comprovantes as $tipo)    
-                  <option value="{{$tipo->codigosistema}}"
+      <div class="row">
+            @isset($inquilinos)
+                  <div class="col-12">
+                        <select name="inquilino">                 
+                                    @foreach ($inquilinos as $inquilino)
+                                          <option value="{{$inquilino->id}}">
+                                                {{$inquilino->nome}}</option>
+                                    @endforeach               
+                        </select>
+                  </div>
+            @endisset     
+      </div>
+      <div class="row" >
+            @isset($tipos_comprovantes)
+                  <div class="col-6">
+                        <select name="tipo-comprovante">
+                              @foreach ($tipos_comprovantes as $tipo)    
+                              <option value="{{$tipo->codigosistema}}"
+            
+                              @isset($comprovante->tipocomprovante)
+                                    @if($comprovante->tipocomprovante == $tipo->codigosistema) selected @endif
+                              @endisset
+            
+                              >{{$tipo->tipo}}</option>
+                              @endforeach
+                        </select>
+                  </div>
+            @endisset
+            <div class="col-6">
+                  <input name="valor-comprovante" id="input-valor-comprovante" type="text" placeholder="Valor do comprovante: "
+                  value="{{ isset($comprovante->valor) ?
+                  old('valor-comprovante', $comprovante->valor) : old('valor-comprovante') }}">
+            </div>
 
-                  @isset($comprovante->tipocomprovante)
-                        @if($comprovante->tipocomprovante == $tipo->codigosistema) selected @endif
-                  @endisset
+      </div>
+      <div class="row">
+            <div class="col-12">
+                  <textarea name="descricao" rows="3" cols="12" placeholder="Observações sobre o comprovante: " >
+                        {{ isset($comprovante->descricao) ? 
+                        old('descricao', $comprovante->descricao) : old('descricao') }}
+                  </textarea>
+            </div>
+      </div>
 
-                  >{{$tipo->tipo}}</option>
-                  @endforeach
-            </select>
-      @endisset
-      <br>
-      <textarea name="descricao" rows="3" cols="12" placeholder="Observações sobre o comprovante: " >
-            {{ isset($comprovante->descricao) ? 
-            old('descricao', $comprovante->descricao) : old('descricao') }}
-      </textarea>
-
-      @isset($inquilinos)
-            <select name="inquilino">                 
-                        @foreach ($inquilinos as $inquilino)
-                              <option value="{{$inquilino->id}}">
-                                    {{$inquilino->nome}}</option>
-                        @endforeach               
-            </select>
-      @endisset
-
-      <br>
-      <input name="data-comprovante" id="data-input" type="text" placeholder="Data formato AAAA-mm-dd: "
-      value="{{ isset($comprovante->dataComprovante) ? 
-            old('data-comprovante', $comprovante->dataComprovante) : 
-                  old('data-comprovante') }}">
-      <br>
-      <input name="referencia" id="ano-mes-input" type="text" placeholder="Ano/mês da referência: "
-      value="{{ isset($comprovante->referencia) ?
-            old('referencia', $comprovante->referencia) : old('referencia') }}">
-      <button type="submit">OK</button>
+      <div class="row">
+            <div class="col-6">
+            <input name="data-comprovante" id="data-input" type="text" placeholder="Data formato AAAA-mm-dd: "
+            value="{{ isset($comprovante->dataComprovante) ? 
+                  old('data-comprovante', $comprovante->dataComprovante) : 
+                        old('data-comprovante') }}">
+            </div>
+            <div class="col-6">
+            <input name="referencia" id="ano-mes-input" type="text" placeholder="Ano/mês da referência: "
+                  value="{{ isset($comprovante->referencia) ?
+                  old('referencia', $comprovante->referencia) : old('referencia') }}">
+            </div>
+      </div>
+      <div class="row">
+            <div class="col-4">
+                  <button class="button confirmacao-button" type="submit">OK</button>
+            </div>
+      </div>
 </form>
