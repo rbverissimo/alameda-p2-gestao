658190743aa036c5b0e8d4ea1dba6098536f78b7
rbverissimo <rverissimo.dev@gmail.com>
2024-04-20 18:57:24 -0300

    Pensando sobre a tabela no mobile

--- resources/views/app/imoveis.blade.php
+++ resources/views/app/imoveis.blade.php
@@ -2,10 +2,8 @@
 
 @section('conteudo')
       <div class="row">
-            <div class="col-12">
-                  @component('app.layouts._components.lista_imoveis_component',
-                  compact('imoveis'))
-                  @endcomponent
-            </div>
+            @component('app.layouts._components.lista_imoveis_component',
+            compact('imoveis'))
+            @endcomponent
       </div>
 @endsection
--- resources/views/app/layouts/_components/lista_imoveis_component.blade.php
+++ resources/views/app/layouts/_components/lista_imoveis_component.blade.php
@@ -1,27 +1,28 @@
-<table>
-      <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Endereço</th>
-            <th>Qtde. Inquilinos</th>
-            <th>Ações</th>
-      </tr>
-      @foreach ($imoveis as $i)
-            <tr class="table-row">
-                  <td>
-                  <a class="table-link">{{$i->id}}</a>
-                  </td>
-                  <td>
-                  <a class="table-link">{{$i->nomefantasia}}</a>
-                  </td>
-                  <td>
-                  <a class="table-link">{{$i->endereço}}</a>
-                  </td>
-                  <td>
-                  <a class="table-link">{{$i->qtdeInquilinos}}</a>
-                  </td>
-                  <td>
-                  </td>
+
+      <table>
+            <tr>
+                  <th>ID</th>
+                  <th>Nome</th>
+                  <th>Endereço</th>
+                  <th>Qtde. Inquilinos</th>
+                  <th>Ações</th>
             </tr>
-      @endforeach
-</table>
+            @foreach ($imoveis as $i)
+                  <tr class="table-row">
+                        <td>
+                        <a class="table-link">{{$i->id}}</a>
+                        </td>
+                        <td>
+                        <a class="table-link">{{$i->nomefantasia}}</a>
+                        </td>
+                        <td>
+                        <a class="table-link">{{$i->endereço}}</a>
+                        </td>
+                        <td>
+                        <a class="table-link">{{$i->qtdeInquilinos}}</a>
+                        </td>
+                        <td>
+                        </td>
+                  </tr>
+            @endforeach
+      </table>
