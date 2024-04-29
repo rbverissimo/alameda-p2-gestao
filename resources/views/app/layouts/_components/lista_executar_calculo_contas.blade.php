a0ae72da4aef1816a762819453fe0fa0261fdf94
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2024-04-29 14:12:36 -0300

    Construindo o painel do cálculo

---
+++ resources/views/app/layouts/_components/lista_executar_calculo_contas.blade.php
@@ -0,0 +1,31 @@
+<div id="table-wrapper">
+    <div class="row">
+          <div class="col-12 scrollable-table end-table-scroll">     
+                <table id='lista-contas'>
+                      <tr>
+                            <th>ID</th>
+                            <th>Valor</th>
+                            <th>Tipo</th>
+                            <th>Sala</th>
+                            <th>Ações</th>
+                      </tr>
+                      @foreach ($contas_imovel as $conta)
+                          <tr class="table-row">
+                            <td><a class="table-link">{{ $conta->id}}</a></td>
+                            <td><a class="table-link">{{ $conta->valor}}</a></td>
+                            <td><a class="table-link">{{ $conta->tipocodigo}}</a></td>
+                            <td><a class="table-link">{{ $conta->nomesala}}</a></td>
+                            <td>
+                                <div class="col-3">
+                                    <img class="crud-icon" src="{{asset("icons/edit-icon.svg")}}" alt="EDITAR">
+                                </div>
+                                <div class="col-3">
+                                    <img class="crud-icon" src="{{asset("icons/delete-icon.svg")}}" alt="EXCLUIR">
+                                </div>
+                            </td>
+                          </tr>
+                      @endforeach
+                </table>
+          </div>
+    </div>
+</div>
--- resources/views/app/painel-calcular-contas.blade.php
+++ resources/views/app/painel-calcular-contas.blade.php
@@ -1,10 +1,21 @@
 @extends('app.layouts.template-basico')
 
 @section('conteudo')
-    <div class="row">
+    <div class="row center-itens">
         <div class="col-3">
             @include('app.layouts._partials.simple-carousel')
         </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            @include('app.layouts._components.lista_executar_calculo_contas')
+        </div>
+    </div>
+    <div class="row">
+        NESSA DIV ESTARÁ O BOTÃO QUE DARÁ INÍCIO AO CÁLCULO. ELE ATIVARÁ UM MODAL PARA O USUÁRIO CONFIRMAR O CÁLCULO
+    </div>
+    <div class="row">
+        NESSA DIV ESTARÁ O OUTPUT DO CÁLCULO COM UM BOTÃO PARA O USUÁRIO FECHAR A REFERÊNCIA
     </div>
 @endsection
 
