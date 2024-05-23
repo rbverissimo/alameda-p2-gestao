9cd6dfd1f39571805411e0f2129fb47251f5e827
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2024-05-23 06:44:29 -0300

    Commitando mudanças em uma tentativa de organizar os módulos da aplicação

--- public/js/comportamento-dinamico.js
+++ public/js/comportamento-dinamico.js
@@ -1,8 +1,29 @@
+
+/**
+ * Esse método faz o toggle do overlay de carregamento.
+ * Esse overlay possui nele a animação do Spinner e, geralmente, é usado em
+ * requests ao servidor que resultam em alguma mudança no estado da view da aplicação.
+ * O conteúdo geralmente é renderizado dinamicamente;
+ * 
+ * @param {*} loadingOverlay 
+ */
 export function toggleOverlay(loadingOverlay){
     if(loadingOverlay.style.display === 'block'){
         loadingOverlay.style.display = 'none';
     } else {
         loadingOverlay.style.display = 'block';
     }
+}
+
+/**
+ * O intuito de criar essa função é oferecer uma interface única e desacoplável 
+ * para fazer o toggle tanto do overlay quando do wrapper do modal
+ * 
+ * @param {*} overlay 
+ * @param {*} wrapperModal 
+ */
+export function toggleModal(overlay, wrapperModal){
+    overlay.style.display = overlay.style.display === 'none' ? 'block' : 'none';
+    wrapperModal.style.display = wrapperModal.style.display === 'none' ? 'block' : 'none';
 }
 
