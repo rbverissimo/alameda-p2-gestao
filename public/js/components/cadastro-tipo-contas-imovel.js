23f2e80e0402f00f6e74c692e039081e14a69e5b
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2024-06-18 07:17:40 -0300

    Retirando um console.log

--- public/js/components/cadastro-tipo-contas-imovel.js
+++ public/js/components/cadastro-tipo-contas-imovel.js
@@ -1,7 +1,5 @@
 const chipGroupLabels = document.querySelectorAll('.chip-group label');
 
-console.log(chipGroupLabels);
-
   chipGroupLabels.forEach(label => {
     label.addEventListener('click', function() {
       const checkbox = this.querySelector('input[type="checkbox"]');
