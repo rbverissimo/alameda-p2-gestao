b8d979df1e4a3e746bb097ddb121b431c23458ce
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2024-06-25 07:02:20 -0300

    Implementando o método que busca os fornecedores do banco de dados de forma assíncrona. Resolvido o bug do scripts.js.

--- public/js/views/cadastro-compras.js
+++ public/js/views/cadastro-compras.js
@@ -1,7 +1,21 @@
-import { handleBackspaceHyphen } from "/app/public/js/scritps.js";
-/* import { dataMascara } from './public/js/scripts.js';
-import { isDataValida } from './public/js/scripts.js';
-import { apenasNumeros } from './public/js/scripts.js';
-import { mascaraValoresEmReal } from './public/js/scripts.js';
- */
-console.log('funcionando');
+export var fornecedores;
+
+document.addEventListener('DOMContentLoaded', () => {
+    buscarFornecedores();
+});
+
+async function buscarFornecedores(){
+    try {
+        const response = await fetch('/fornecedores/buscar');
+        if(!response.ok){
+            throw new Error(`Erro ao buscar as informações no servidor: ${response.status}`);
+        }
+        const data = await response.json();
+        if(data !== null ){
+            fornecedores = data;
+        }
+    } catch (error) {
+        showMensagem(error, 'falha', 5000);
+        console.log(error);
+    }
+}
