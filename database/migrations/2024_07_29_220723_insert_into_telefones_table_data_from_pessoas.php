899f6c7f9d498668cce6f8ec3d74c415d24ca0ff
rbverissimo <rverissimo.dev@gmail.com>
2024-07-29 19:36:51 -0300

    Migration dos dados da tabela de pessoas implementado

---
+++ database/migrations/2024_07_29_220723_insert_into_telefones_table_data_from_pessoas.php
@@ -0,0 +1,85 @@
+<?php
+
+use App\Models\Pessoa;
+use App\Models\Telefone;
+use App\Utils\ProjectUtils;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class InsertIntoTelefonesTableDataFromPessoas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::transaction(function($closure){
+
+            $pessoas = Pessoa::all();
+
+            foreach ($pessoas as $pessoa) {
+                
+                if($this->isTelefoneValido($pessoa->telefone_fixo)){
+                    $telefone_valido = $this->converterTelefone($pessoa->telefone_fixo);
+                    Telefone::create([
+                        'pessoa_id' => $pessoa->id,
+                        'ddd' => $telefone_valido['ddd'],
+                        'telefone' => $telefone_valido['telefone'],
+                        'tipo_telefone' => 1000
+                    ]);
+                }
+
+                if($this->isTelefoneValido($pessoa->telefone_celular)){
+                    $telefone_valido = $this->converterTelefone($pessoa->telefone_celular);
+                    Telefone::create([
+                        'pessoa_id' => $pessoa->id,
+                        'ddd' => $telefone_valido['ddd'],
+                        'telefone' => $telefone_valido['telefone'],
+                        'tipo_telefone' => 1010
+                    ]);
+                }
+
+                if($this->isTelefoneValido($pessoa->telefone_trabalho)){
+                    $telefone_valido = $this->converterTelefone($pessoa->telefone_trabalho);
+                    Telefone::create([
+                        'pessoa_id' => $pessoa->id,
+                        'ddd' => $telefone_valido['ddd'],
+                        'telefone' => $telefone_valido['telefone'],
+                        'tipo_telefone' => 1020
+                    ]);
+                }
+            }
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::transaction(function($closure){
+            $pessoas = Pessoa::pluck('id');
+            foreach ($pessoas as $pessoa) {
+                DB::delete('DELETE FROM TELEFONES WHERE PESSOA_ID = ?', [$pessoa]);
+            }
+        });
+    }
+
+    private function converterTelefone($telefone){
+        $ddd = substr(ProjectUtils::tirarMascara($telefone), 0, 2);
+        $telefone_sem_ddd = substr(ProjectUtils::tirarMascara($telefone), 2);
+        return [ 'ddd' => $ddd, 'telefone' => $telefone_sem_ddd];
+    }
+
+    private function isTelefoneValido($telefone){
+        $telefone_string = ProjectUtils::tirarMascara($telefone);
+        return $telefone_string !== null && $telefone_string !== '';
+    }
+}
--- database/seeders/PessoaSeeder.php
+++ database/seeders/PessoaSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Pessoa;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PessoaSeeder extends Seeder
 {
@@ -14,30 +15,49 @@ class PessoaSeeder extends Seeder
      */
     public function run()
     {
-        Pessoa::create([
-            'nome' => 'Agmar',
-            'telefone_celular' => '(62)98134-0903'
-        ]);
 
-        Pessoa::create([
-            'nome' => 'Branca',
-            'telefone_celular' => '(62)98204-6595'
-        ]);
+        DB::transaction(function($closure){
+            Pessoa::create([
+                'nome' => 'Agmar',
+                'telefone_celular' => '(62)98134-0903'
+            ]);
+    
+            Pessoa::create([
+                'nome' => 'Branca',
+                'telefone_celular' => '(62)98204-6595'
+            ]);
+    
+            Pessoa::create([
+                'nome' => 'Ezequias',
+                'telefone_celular' => '(62)99460-5658'
+            ]);
+    
+            Pessoa::create([
+                'nome' => 'Igor',
+                'telefone_celular' => '(62)98143-2680'
+            ]);
+    
+            Pessoa::create([
+                'nome' => 'Paulo',
+                'telefone_celular' => '(62)99213-7439' 
+            ]);
 
-        Pessoa::create([
-            'nome' => 'Ezequias',
-            'telefone_celular' => '(62)99460-5658'
-        ]);
+            Pessoa::create([
+                'nome' => 'Roberto Teste Oliveira',
+                'telefone_fixo' => '6234563455',
+                'telefone_celular' => '62996694548',
+                'profissao' => 'Pedreiro',
+                'cpf' => '32445322146'
+            ]);
 
-        Pessoa::create([
-            'nome' => 'Igor',
-            'telefone_celular' => '(62)98143-2680'
-        ]);
-
-        Pessoa::create([
-            'nome' => 'Paulo',
-            'telefone_celular' => '(62)99213-7439' 
-        ]);
+            Pessoa::create([
+                'nome' => 'Higor Sanches',
+                'telefone_fixo' => '6232334078',
+                'telefone_celular' => '62991214430',
+                'profissao' => 'Pedreiro',
+                'cpf' => '00102232109'
+            ]);
+        });
 
     }
 }
