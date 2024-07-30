6edaf6d8fde44aa1668958ff6d6fa368f99b6b83
Renato <rverissimo.dev@gmail.com>
2024-07-30 14:38:00 -0300

    Encontrado um workaround para as foreign key constraints

---
+++ database/migrations/2024_07_30_164855_alter_inquilinos_table_add_columns_pessoa.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AlterInquilinosTableAddColumnsPessoa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('inquilinos', function(Blueprint $table){
+            $table->string('nome', 60)->nullable();
+            $table->string('cpf', 11)->nullable();
+            $table->string('profissao', 60)->nullable();
+            $table->unsignedBigInteger('endereco_trabalho')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('PRAGMA foreign_keys = OFF;');
+        DB::transaction(function(){
+
+            Schema::table('inquilinos', function(Blueprint $table){
+                $table->dropColumn('nome');
+            });
+
+            Schema::table('inquilinos', function(Blueprint $table){
+                $table->dropColumn('profissao');
+            });
+
+            Schema::table('inquilinos', function(Blueprint $table){
+                $table->dropColumn('cpf');
+            });
+
+            Schema::table('inquilinos', function(Blueprint $table){
+                $table->dropColumn('endereco_trabalho');
+            });
+        });
+    }
+}
--- database/seeders/InquilinosAlugueisSeeder.php
+++ database/seeders/InquilinosAlugueisSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\InquilinoAluguel;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class InquilinosAlugueisSeeder extends Seeder
 {
@@ -14,53 +15,56 @@ class InquilinosAlugueisSeeder extends Seeder
      */
     public function run()
     {
-        $i = InquilinoAluguel::create([
-            'inquilino' => 1,
-            'valorAluguel' => 330.0,
-            'inicioValidade' => 202302,
-            'fimValidade' => 202501,
-            'reajuste_previsto' => 4.5
-        ]);
 
-        $i->save();
-
-        $i2 = InquilinoAluguel::create([
-            'inquilino' => 2,
-            'valorAluguel' => 550.0,
-            'inicioValidade' => 202302,
-            'fimValidade' => 202501,
-            'reajuste_previsto' => 4.0
-        ]);
-
-        $i2->save();
-
-        $i3 = InquilinoAluguel::create([
-            'inquilino' => 3,
-            'valorAluguel' => 440.0,
-            'inicioValidade' => 202302,
-            'reajuste_previsto' => 0.0
-        ]);
-
-        $i3->save();
-
-        $i4 = InquilinoAluguel::create([
-            'inquilino' => 4,
-            'valorAluguel' => 600.0,
-            'inicioValidade' => 202308,
-            'fimValidade' => 202501,
-            'reajuste_previsto' => 3.0
-        ]);
-
-        $i4->save();
-
-        $i5 = InquilinoAluguel::create([
-            'inquilino' => 5,
-            'valorAluguel' => 550.0,
-            'inicioValidade' => 202302,
-            'fimValidade' => 202501,
-            'reajuste_previsto' => 5.0
-        ]);
-
-        $i5->save(); 
+        DB::transaction(function ($closure) {
+            $i = InquilinoAluguel::create([
+                'inquilino' => 1,
+                'valorAluguel' => 330.0,
+                'inicioValidade' => 202302,
+                'fimValidade' => 202501,
+                'reajuste_previsto' => 4.5
+            ]);
+    
+            $i->save();
+    
+            $i2 = InquilinoAluguel::create([
+                'inquilino' => 2,
+                'valorAluguel' => 550.0,
+                'inicioValidade' => 202302,
+                'fimValidade' => 202501,
+                'reajuste_previsto' => 4.0
+            ]);
+    
+            $i2->save();
+    
+            $i3 = InquilinoAluguel::create([
+                'inquilino' => 3,
+                'valorAluguel' => 440.0,
+                'inicioValidade' => 202302,
+                'reajuste_previsto' => 0.0
+            ]);
+    
+            $i3->save();
+    
+            $i4 = InquilinoAluguel::create([
+                'inquilino' => 4,
+                'valorAluguel' => 600.0,
+                'inicioValidade' => 202308,
+                'fimValidade' => 202501,
+                'reajuste_previsto' => 3.0
+            ]);
+    
+            $i4->save();
+    
+            $i5 = InquilinoAluguel::create([
+                'inquilino' => 5,
+                'valorAluguel' => 550.0,
+                'inicioValidade' => 202302,
+                'fimValidade' => 202501,
+                'reajuste_previsto' => 5.0
+            ]);
+    
+            $i5->save(); 
+        });
     }
 }
