61e3aa134acddff427e342431d637edd18dee3f2
Renato <rverissimo.dev@gmail.com>
2024-08-09 10:18:18 -0300

    Implementando a interface para o crud das notas fiscais

--- resources/views/app/layouts/_components/dados_nota_fiscal_servico.blade.php
+++ resources/views/app/layouts/_components/dados_nota_fiscal_servico.blade.php
@@ -4,12 +4,115 @@
     </div>
     <div class="row">
         <div class="col-4">
+            @php
+                $dataEmissao = isset($nota) ? $nota->getDataEmissao() : null;
+            @endphp
             <x-forms.input
-                label-text="Data da emissão da nota: "
+                label-text="Data da emissão: "
                 attr-name="data-emissao"
                 pattern-name="data-emissao"
-                :data-input="$nota->dataEmissao"
+                :data-input="$dataEmissao"
+            ></x-forms.input>
+        </div>
+        <div class="col-4">
+            @php
+                $valorBruto = isset($nota) ? $nota->getValorBruto() : null;
+            @endphp
+            <x-forms.input
+                label-text="Valor bruto: "
+                attr-name="valor-bruto"
+                pattern-name="valor-bruto"
+                :data-input="$valorBruto"
+            ></x-forms.input>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+            @php
+                $serieNota = isset($nota) ? $nota->getSerie() : null;
+            @endphp
+            <x-forms.input
+                label-text="Série: "
+                attr-name="nota-serie"
+                pattern-name="nota-serie"
+                :data-input="$serieNota"
+            ></x-forms.input>
+        </div>
+        <div class="col-5">
+            @php
+                $numeroDocumento = isset($nota) ? $nota->getNumeroDocumento() : null;
+            @endphp
+            <x-forms.input
+                label-text="Nº do documento: "
+                attr-name="numero-documento"
+                pattern-name="numero-documento"
+                :data-input="$numeroDocumento"
+            ></x-forms.input>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-8">
+            @php
+                $servico = isset($nota) ? $nota->getTipoServico() : null;
+                $tipos_servicos = [];
+            @endphp
+            <x-forms.select
+                label-text="Selecione o tipo do serviço prestado: "
+                pattern-name="tipos-servicos-select"
+                :collection="$tipos_servicos"
+                :selected-value="$servico"
+            ></x-forms.select>
+        </div>
+    </div>
+    <div class="dashboard light-dashboard">
+        <div class="divisor-header secondary-divisor">Informações à cerca das retenções</div>
+        <div class="row">
+            <div class="col-2">
+                @php
+                    $aliquota = isset($nota) ? $nota->getAliquota() : null;
+                @endphp
+                <x-forms.input
+                    label-text="Alíquota: "
+                    attr-name="aliquota"
+                    pattern-name="aliquota"
+                    :data-input="$aliquota"
                 ></x-forms.input>
+            </div>
+            <div class="col-4">
+                @php
+                    $valorISS = isset($nota) ? $nota->getValorISS() : null;
+                @endphp
+                <x-forms.input
+                    label-text="Valor ISS: "
+                    attr-name="valor-iss"
+                    pattern-name="valor-iss"
+                    :data-input="$valorISS"
+                ></x-forms.input>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            @php
+                $baseINSS = isset($nota) ? $nota->getBaseINSS() : null;
+            @endphp
+            <x-forms.input
+                label-text="Base INSS: "                
+                attr-name="base-inss"
+                pattern-name="base-inss"
+                :data-input="$baseINSS"
+            ></x-forms.input>
+        </div>
+        <div class="col-4">
+            @php
+                $valorRetido = isset($nota) ? $nota->getValorRetido() : null;
+            @endphp
+            <x-forms.input
+                label-text="Valor retido: "                
+                attr-name="valor-retido"
+                pattern-name="valor-retido"
+                :data-input="$valorRetido"
+            ></x-forms.input>
         </div>
     </div>
 </div>
--- resources/views/app/layouts/_components/form_nota_fiscal_servico.blade.php
+++ resources/views/app/layouts/_components/form_nota_fiscal_servico.blade.php
@@ -5,18 +5,17 @@
         @method('PUT')
     @endisset
 
-    <div class="row">
-        <div class="col-12">
-            @component('app.layouts._components.dados_nota_fiscal_servico', compact('nota'))
-                
-            @endcomponent
-        </div>
-    </div>
-
     <div class="dashboard light-dashboard">
+        <div class="row">
+            <div class="col-12">
+                @component('app.layouts._components.dados_nota_fiscal_servico', compact('nota'))
+                    
+                @endcomponent
+            </div>
+        </div>
         <div class="row center-itens">
-            <div class="col-2">
-                <button class="button confirmacao-button">Salvar nota</button>
+            <div class="col-3">
+                <button class="button confirmacao-button">Salvar</button>
             </div>
         </div>
     </div>
