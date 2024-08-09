7dc271645ba0f798987f48d8e516f43a237dc8c5
Renato <rverissimo.dev@gmail.com>
2024-08-09 16:59:27 -0300

    Tô brisando hoje não vai rolar mais nada não

--- public/js/db/indexed.js
+++ public/js/db/indexed.js
@@ -2,7 +2,11 @@ export function saveInput(dbName, storeName, inputName, inputValue){
     const request = indexedDB.open(dbName, 1);
 
     request.onsuccess = (event) => {
-        
+
     }
 
+    const data = {
+        name: inputName,
+        value: inputValue
+    }
 }
