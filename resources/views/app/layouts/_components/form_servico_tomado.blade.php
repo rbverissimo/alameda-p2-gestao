9042f0d821c0a0a7c91b290d187de9ba1c1805cf
Renato <rverissimo.dev@gmail.com>
2024-08-12 12:33:34 -0300

    Adicionando o identificador do serviço ao cadastro do serviço tomado

--- resources/views/app/layouts/_components/form_servico_tomado.blade.php
+++ resources/views/app/layouts/_components/form_servico_tomado.blade.php
@@ -5,6 +5,38 @@
   @if (isset($servico->id))
       @method('PUT')
   @endif
+  <div class="dashboard light-dashboard">
+    <div class="divisor-header secondary-divisor">
+      Identificação do serviço tomado
+    </div>
+    <div class="row">
+      <div class="col-4">
+        @php
+            $codigoServico = null;
+        @endphp
+        <x-forms.input
+          label-text="Defina um código: "
+          pattern-name="codigo-servico"
+          attr-name="codigo-servico"
+          placeholder="Ex: 1, 1010, 2010 etc "
+          :data-input="$codigoServico"
+        >
+        </x-forms.input>
+      </div>
+      <div class="col-8">
+        @php
+            $nomeServico = null;
+        @endphp
+        <x-forms.input
+          label-text="Indique um nome para o serviço: "
+          pattern-name="nome-servico"
+          attr-name="nome-servico"
+          :data-input="$nomeServico"
+        >
+        </x-forms.input>
+      </div>
+    </div>
+  </div>
   @component('app.layouts._components.dados_servico_tomado', compact('tipos_servicos', 'imoveis'))
       
   @endcomponent
